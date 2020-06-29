f52f984b56edae6b83fc0a849b51aa5dd1a44d5d
vlvanchin <vlvanchin@gmail.com>
2020-06-29 09:24:04 +0800

    sonartype start script update
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>

--- learn_sonatype/start-nexus.sh
+++ learn_sonatype/start-nexus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 # to run the docker nexus
-docker run -d -p 8081:8081 --name nexus -v nexus-data:/nexus-data sonatype/nexus3
+docker run -d -p 8081:8081 --name nexus --rm -v nexus-data:/nexus-data sonatype/nexus3
 

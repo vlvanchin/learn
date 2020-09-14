4565cbc38187445980f2d9321f6eb61a8c475c36
vlvanchin <vlvanchin@gmail.com>
2020-09-14 23:40:59 +0800

    added azure and spring samples
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>

---
+++ learn_azure/demoAzureAdOauth2/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import com.microsoft.azure.spring.autoconfigure.aad.AADAuthenticationFilter;
+
+@EnableOAuth2Sso
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private AADAuthenticationFilter aadAuthFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/api/**").authenticated();
+
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+        http.addFilterBefore(aadAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
---
+++ learn_azure/demoAzureAdOauth2/src/main/resources/static/App/Scripts/app.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+angular.module('todoApp', ['ngRoute', 'AdalAngular'])
+    .config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+        $routeProvider.when("/Home", {
+            controller: "homeCtrl",
+            templateUrl: "/App/Views/Home.html",
+        }).when("/TodoList", {
+            controller: "todoListCtrl",
+            templateUrl: "/App/Views/TodoList.html",
+            requireADLogin: true,
+        }).when("/UserData", {
+            controller: "userDataCtrl",
+            templateUrl: "/App/Views/UserData.html",
+        }).otherwise({redirectTo: "/Home"});
+
+        adalProvider.init(
+            {
+                instance: 'https://login.microsoftonline.com/',
+                tenant: 'vlvanchin.onmicrosoft.com',
+                clientId: 'e3cfe2d7-4e63-4106-8e3f-d3d058ef7c29',
+                cacheLocation: 'localStorage',
+            },
+            $httpProvider
+        );
+
+    }]);

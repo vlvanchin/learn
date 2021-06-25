488a485346375cbd8a3acf6fef494b380063bc88
vlvanchin <vlvanchin+git@gmail.com>
2021-06-25 22:18:12 +0800

    added a new rest endpoint
    Signed-off-by: vlvanchin <vlvanchin+git@gmail.com>

--- learn_azure/azure-ad-demo1/src/main/java/com/van/azure/springbootsampleactivedirectorybackend/security/controller/HelloController.java
+++ learn_azure/azure-ad-demo1/src/main/java/com/van/azure/springbootsampleactivedirectorybackend/security/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.van.azure.springbootsampleactivedirectorybackend.security.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.ui.Model;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
@@ -22,6 +25,24 @@ public class HelloController {
    @RequestMapping("/greeting")
    public String greeting() {
 	   return "Hello there!, <br/>Good morning!";
+   }
+   
+   @RequestMapping(value = "/Policies", method = RequestMethod.GET)
+   public String getPolicy(Authentication authentication) {
+	   System.out.println(authentication);
+	   Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+	   System.out.println(authorities);
+	   DefaultOidcUser principal =  (DefaultOidcUser) authentication.getPrincipal();
+	   System.out.println(principal.getName());
+	   //       UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+//       System.out.println("Principal: " + userPrincipal.getSubject());        
+//
+//       Map<String, Object> map = new LinkedHashMap<>();
+//       map = (Map<String, Object>) userPrincipal.getClaims();
+//
+//       System.out.println("Username: " + map.get("name"));
+//       System.out.println("Email: " + map.get("upn"));
+       return "Hello";
    }
    
 }
